c1ec765c35349e8a671b3429d28e5c70b8634ae5
Vitor Camacho <vitor.camachoo@gmail.com>
2022-10-17 10:54:16 +0100

    Add daisyui library

--- apps/landing-page/app/routes/index.tsx
+++ apps/landing-page/app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1 className="text-3xl font-bold">Tailwind is working!!</h1>
+      <button className="btn btn-sm">Button</button>
     </div>
   );
 }
--- apps/landing-page/tailwind.config.js
+++ apps/landing-page/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+    // themes: ["light"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
 };
