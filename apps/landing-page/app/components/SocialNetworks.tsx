da9203ec2e3998e7869d2907d863a46226b00cbc
Vitor Camacho <vitor.camachoo@gmail.com>
2022-10-20 14:35:01 +0100

    Add social network component

--- apps/landing-page/app/components/SocialNetworks.tsx
+++ apps/landing-page/app/components/SocialNetworks.tsx
@@ -1,26 +1,36 @@
 import { SocialIcon } from "react-social-icons";
 
+export const Facebook = () => (
+  <SocialIcon
+    network="facebook"
+    url="https://www.facebook.com/Citybubbles"
+    fgColor="currentColor"
+    bgColor="transparent"
+  />
+);
+export const Instagram = () => (
+  <SocialIcon
+    network="instagram"
+    url="https://www.instagram.com/citybubblesmadeira"
+    fgColor="currentColor"
+    bgColor="transparent"
+  />
+);
+export const Youtube = () => (
+  <SocialIcon
+    network="youtube"
+    url="https://www.youtube.com/channel/UCGNolJ8F5ngm2eSRNAMVFUA"
+    fgColor="currentColor"
+    bgColor="transparent"
+  />
+);
+
 export const SocialNetworks = () => {
   return (
     <div className="flex flex-row gap-2">
-      <SocialIcon
-        network="facebook"
-        url="https://www.facebook.com/Citybubbles"
-        fgColor="black"
-        bgColor="transparent"
-      />
-      <SocialIcon
-        network="instagram"
-        url="https://www.instagram.com/citybubblesmadeira"
-        fgColor="black"
-        bgColor="transparent"
-      />
-      <SocialIcon
-        network="youtube"
-        url="https://www.youtube.com/channel/UCGNolJ8F5ngm2eSRNAMVFUA"
-        fgColor="black"
-        bgColor="transparent"
-      />
+      <Facebook />
+      <Instagram />
+      <Youtube />
     </div>
   );
 };
