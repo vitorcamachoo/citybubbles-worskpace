eafd178816f630791d00e7596de6e812259685ab
Vitor Camacho <vitor.camachoo@gmail.com>
2022-10-20 14:35:37 +0100

    Extract navigation configuration

---
+++ apps/landing-page/app/config/navigation.ts
@@ -0,0 +1,20 @@
+export const navigation = {
+  main: [
+    { name: "company", href: "/about-us" },
+    { name: "routes", href: "/routes" },
+    { name: "media", href: "/media" },
+    { name: "contact", href: "/contact-us" },
+  ],
+  footer: [
+    { name: "about", href: "/about-us" },
+    { name: "blog", href: "/blog" },
+    { name: "routes", href: "/routes" },
+    { name: "media", href: "/media" },
+    { name: "contact", href: "/contact-us" },
+  ],
+  subfooter: [
+    { name: "terms & conditions", href: "/about-us" },
+    { name: "privacy policy", href: "/privacy-policy" },
+    { name: "eu award", href: "/eu-award" },
+  ],
+};
